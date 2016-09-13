e7257b298bf6f8bab7afd1474ba8f64035c885be
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-09-13 04:15:45 +0100

    have a working mvp in the shape of a one handed theremin. tweaks to follow

--- js/app.js
+++ js/app.js
@@ -7,20 +7,18 @@ var filter;
 var filter2;
 var microphone;
 var oscillator;
+var oscGain;
 var gainNode;
 var delay;
 var source;
 var merger;
+var compressor;
 
-var videoStream = null;
-var video = document.getElementById("video");
+var canvas;
+var ctx;
 
-var audio = new Audio();
-audio.src = 'track2.mp3';
-audio.controls = true;
-audio.loop = true;
-audio.autoplay = false;
-audio.volume = 0.1;
+var analyser;
+
 
 var leapController;
 var handPositionExport =[];
@@ -30,13 +28,18 @@ window.addEventListener('load', init, false);
 
 function init() {
 
- 
+  canvas = document.getElementById('visualizer');
+  ctx = canvas.getContext('2d');
+
+ gradient = ctx.createLinearGradient(0,0,0,300);
+   gradient.addColorStop(1,'#000000');
+   gradient.addColorStop(0.75,'#970fe0');
+   gradient.addColorStop(0.25,'#64efaa');
+   gradient.addColorStop(0,'#ffffff');
 
 //set the window audio context. 
 
 var audioContext = new AudioContext();
-
-document.getElementById('audio_box').appendChild(audio);
 
 //DOM elements
 slider = document.getElementById("slider");
@@ -61,39 +64,59 @@ if(slider2.addEventListener){
   slider2.attachEvent("onchange",onChange2);
 }
 
-
 oscillator = audioContext.createOscillator();
+oscGain = audioContext.createGain();
 oscillator.type = 'sawtooth';
+oscGain.gain.value = 0.5;
+oscillator.frequency.value = 8000;
 
+gainNode= audioContext.createGain();
+gainNode.gain.value = 0.8;
+delay = audioContext.createDelay();
+delay.delayTime.value = 0.5;
 
+analyser = audioContext.createAnalyser();
+// analyser.smoothingTimeConstant = 0.5;
+analyser.fftSize = 512;
 
+compressor = audioContext.createDynamicsCompressor();
+  compressor.threshold.value = -50;
+  compressor.knee.value = 40;
+  compressor.ratio.value = 12;
+  compressor.reduction.value = -20;
+  compressor.attack.value = 0;
+  compressor.release.value = 0.25;
 
 
  document.getElementById('button-play-gum').addEventListener('click', function() {
 
 
-  setInterval(function(){ console.log(handPositionExport[0]); }, 1000);  
+  // setInterval(function(){ console.log(handPositionExport[0]); }, 1000);  
 
       // var negative = -23, 
       // positive = -negative>0 ? -negative : negative;
 
-     
-     source = audioContext.createMediaElementSource(audio); 
 
      merger = audioContext.createChannelMerger(2);
 
 
      setInterval(function(){
-        oscillator.frequency.value = Math.abs(handPositionExport[0]);
-      },100)
+      // if(handPositionExport[0] === !null ){
+        oscillator.frequency.value = Math.abs(handPositionExport[0]*25);
+        // console.log(handPositionExport[1]);
+      
+      },50)
 
+     setInterval(function(){
+        //DO NOT CHANGE DIVISION FACTOR TO BELOW 300
+            oscGain.gain.value = handPositionExport[1]/500;
+     },50)
+
+     setInterval(function(){
+        
+            delay.delayTime.value = Math.abs(handPositionExport[2]/500);
+     },50)
      
-
-     gainNode= audioContext.createGain();
-     gainNode.gain.value = 0.8;
-
-     delay = audioContext.createDelay();
-     delay.delayTime.value = 0.5;
 
      delay.connect(gainNode);
      gainNode.connect(delay);
@@ -110,13 +133,15 @@ oscillator.type = 'sawtooth';
 
      oscillator.start();
      
-     source.connect(delay);
+     oscillator.connect(delay);
+     delay.connect(oscGain);
+     oscillator.connect(oscGain);
+     oscGain.connect(merger,0,0);
+     merger.connect(compressor);
+     compressor.connect(analyser);
+     analyser.connect(audioContext.destination);
 
-     delay.connect(merger,0,1);
-     oscillator.connect(merger,0,0);
-     merger.connect(audioContext.destination);
-
-    
+    animateVisualizer();
      
     
 
@@ -140,6 +165,29 @@ function onChange(){
 function onChange2(){
   filter2.frequency.value = slider2.value;
   freqDisplay2.innerHTML = slider2.value;
+}
+
+function animateVisualizer(){
+  window.requestAnimationFrame(animateVisualizer);
+  data_array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(data_array);
+  clearCanvas();
+
+  ctx.fillStyle = gradient; // Color of the bars change the math below to tweak number of bars.
+  bars = 200;
+    for (var i = 0; i < bars; i++) {
+    bar_x = i * 2;
+    bar_width = 1;
+
+    //below line sets the height of the bar
+    bar_height = -(data_array[i] / 2);
+    //  fillRect( x, y, width, height ) // Explanation of the parameters below
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+    }
+}
+
+function clearCanvas(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
--- js/leap-custom.js
+++ js/leap-custom.js
@@ -13,9 +13,9 @@ leapController = Leap.loop({enableGestures:true}, function(frame){
 
                             // console.log(hand.type, hand.palmPosition);}
 
-                            var xPos = hand.palmPosition[0] * 50;
-                            var yPos = hand.palmPosition[1] * 50;
-                            var zPos = hand.palmPosition[2] * 50;
+                            var xPos = hand.palmPosition[0];
+                            var yPos = hand.palmPosition[1];
+                            var zPos = hand.palmPosition[2];
 
                             // console.log(xPos);
 
@@ -24,4 +24,11 @@ leapController = Leap.loop({enableGestures:true}, function(frame){
                             // return handPositionExport;
                         };
 
+
+
                 });
+
+
+//set up for a theremin
+//right hand y axis for gain, left hand x-axis for frequency
+//change scale so more powerful gain on top and bottom end.
